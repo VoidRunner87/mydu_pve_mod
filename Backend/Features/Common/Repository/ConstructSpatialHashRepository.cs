fd3c3d7999b4cf9d99f04e42e4a69d258a26a0c0
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-09-29 18:23:54 -0700

    Added rules to blind npcs from Warp Anchors

--- Backend/Features/Common/Repository/ConstructSpatialHashRepository.cs
+++ Backend/Features/Common/Repository/ConstructSpatialHashRepository.cs
@@ -30,6 +30,7 @@ public class ConstructSpatialHashRepository(IServiceProvider serviceProvider) : 
             LEFT JOIN public.ownership O ON (C.owner_entity_id = O.id)
             WHERE C.sector_x = @x AND C.sector_y = @y AND C.sector_z = @z AND
                   C.deleted_at IS NULL AND
+                  C.json_properties->>'isUntargetable' = 'false' AND
                   C.owner_entity_id IS NOT NULL AND (O.player_id NOT IN({StaticPlayerId.Aphelia}, {StaticPlayerId.Unknown}) OR (O.player_id IS NULL AND O.organization_id IS NOT NULL))
             """,
             new
