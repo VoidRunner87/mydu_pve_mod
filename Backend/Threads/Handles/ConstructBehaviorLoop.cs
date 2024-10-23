38ed04ef765840e6375313436dce3d5d07525619
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-10-23 09:32:43 -0700

    Added Handle Scope

--- Backend/Threads/Handles/ConstructBehaviorLoop.cs
+++ Backend/Threads/Handles/ConstructBehaviorLoop.cs
@@ -67,6 +67,12 @@ public class ConstructBehaviorLoop : HighTickModLoop
             
             foreach (var kvp in ConstructHandles)
             {
+                using var constructScope = _logger.BeginScope(new Dictionary<string, object>
+                {
+                    {nameof(kvp.Value.ConstructId), kvp.Value.ConstructId},
+                    {"ConstructHandleId", kvp.Value.Id},
+                });
+                
                 var task = Task.Run(() => RunIsolatedAsync(() => TickConstructHandle(deltaTime, kvp.Value)));
                 taskList.Add(task);
             }
