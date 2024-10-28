50f0b70a36dd3a87372ab9bc41dfd415889c5b1e
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-10-28 11:14:07 -0700

    Increasing Missions Payout

--- Backend/Features/Quests/Services/MissionProceduralGenerationConfig.cs
+++ Backend/Features/Quests/Services/MissionProceduralGenerationConfig.cs
@@ -5,6 +5,6 @@ namespace Mod.DynamicEncounters.Features.Quests.Services;
 public static class MissionProceduralGenerationConfig
 {
     public static readonly TimeSpan TimeFactor = TimeSpan.FromMinutes(50);
-    public const float TransportQuantaMultiplier = 1.1f;
-    public const float ReverseTransportMultiplier = 1.3f;
+    public const float TransportQuantaMultiplier = 1.6f;
+    public const float ReverseTransportMultiplier = 2.7f;
 }
