82e52db402d874ea216176aae286ba4fc023f9e6
Pedro Matos <voidrunner87@users.noreply.github.com>
2024-10-29 16:45:10 -0700

    Timer to report Heartbeat

--- Backend/Threads/Handles/CommandHandlerLoop.cs
+++ Backend/Threads/Handles/CommandHandlerLoop.cs
@@ -8,6 +8,7 @@ using Mod.DynamicEncounters.Features.Party.Interfaces;
 using Mod.DynamicEncounters.Helpers;
 using NQ;
 using NQutils.Exceptions;
+using Timer = System.Timers.Timer;
 
 namespace Mod.DynamicEncounters.Threads.Handles;
 
@@ -18,19 +19,21 @@ public class CommandHandlerLoop(IThreadManager threadManager, CancellationToken 
     private readonly ILogger<CommandHandlerLoop> _logger = ModBase.ServiceProvider.CreateLogger<CommandHandlerLoop>();
     private readonly IPlayerPartyCommandHandler _commandHandler =
         ModBase.ServiceProvider.GetRequiredService<IPlayerPartyCommandHandler>();
+    private readonly Timer _timer = new Timer(TimeSpan.FromSeconds(1)); 
     
     public override async Task Tick()
     {
         try
         {
+            _timer.Elapsed += (_, _) => ReportHeartbeat();
+            _timer.Start();
+            
             var messageContent =
                 await _listener.GetLastEventWait(CanHandleMessage, 60000);
 
             await _commandHandler.HandleCommand(messageContent.fromPlayerId, messageContent.message);
 
             _listener.Clear();
-
-            ReportHeartbeat();
         }
         catch (BusinessException bex)
         {
@@ -38,14 +41,12 @@ public class CommandHandlerLoop(IThreadManager threadManager, CancellationToken 
 
             await ModBase.Bot.Reconnect();
         }
-        catch (EventNotFoundException)
-        {
-            ReportHeartbeat();
-        }
         catch (Exception e)
         {
             _logger.LogError(e, "Failure on listening to commands");
         }
+        
+        _timer.Stop();
     }
 
     private bool CanHandleMessage(MessageContent mc)
